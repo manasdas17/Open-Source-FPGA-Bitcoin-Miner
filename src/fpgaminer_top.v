668a63694bbbcc36cf561629934022bce6a1ce75
Udi Finkelstein <github@udif.com>
2011-06-05 01:44:49 +0300

    Loop parameter doesn't need the LOOP=1 special case
    Should be handled by the general case just as well.

--- src/fpgaminer_top.v
+++ src/fpgaminer_top.v
@@ -121,7 +121,7 @@ module fpgaminer_top (osc_clk);
 		reg reset = 1'b0;	// NOTE: Reset is not currently used in the actual FPGA; for simulation only.
 	`endif
 
-	assign cnt_next =  reset ? 6'd0 : (LOOP == 1) ? 6'd0 : (cnt + 6'd1) & (LOOP-1);
+	assign cnt_next =  reset ? 6'd0 : (cnt + 6'd1) & (LOOP-6'd1);
 	// On the first count (cnt==0), load data from previous stage (no feedback)
 	// on 1..LOOP-1, take feedback from current stage
 	// This reduces the throughput by a factor of (LOOP), but also reduces the design size by the same amount
